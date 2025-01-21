bf594b0e9ea1a999785d2f5b235c5cd785deb26f
husain <husain68644@gmail.com>
2025-01-22 01:18:27 +0545

    first commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 app.use('/user',router)
 app.get('/',(req,res)=>{
   console.log('hello world get function executed')
-  res.send('<h1>hello world123 </h1>')
+  res.send('<h1>hello world..</h1>')
 })
 const port=8000;
 app.listen(port,console.log(`Example app listening on port ${port}`));
