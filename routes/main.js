0586dec9974c4c90125ed6b42ea49dcec25214b7
husain644 <husain68644@gmail.com>
2025-08-12 17:51:13 +0530

    commit

--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import userRouter from './user/user.js'
 import HtmlRouter from '../html_upload/route.js'
+import { NotFound } from '../utils/notFound.js';
 
 const router= express.Router();
 router.use('/user',userRouter);
 router.use('/html',HtmlRouter);
-
+router.use('/',NotFound);
 
 export default router
---
+++ utils/notFound.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import path from 'path'
+import fs from 'fs'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename)
+
+export function NotFound(req,res){
+  res.sendFile(path.join(__dirname, '../static/temp/notfound.html'))
+}
