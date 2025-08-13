440ba757e877dac1d2fd700df9fa32eefff2ab82
husain644 <husain68644@gmail.com>
2025-08-13 09:25:06 +0530

    commit

--- html_upload/controllers/control.js
+++ html_upload/controllers/control.js
@@ -9,8 +9,36 @@ export function AddHtmlFile (req,res){
     res.sendFile(path.join(__dirname, '../html_files/add.html'));
 }
 export function getHtml(req,res){
-    const html=req.body.html
-    const filePath=path.join(__dirname, `../html_files/${req.body.fileName}.html`)
-     fs.writeFileSync(filePath,html)
-     res.send({"reply":'ok'})
+    const html=req.body.html;
+    const fileName=req.body.fileName;
+    const filePath=path.join(__dirname, `../html_files/${fileName}.html`);
+    if (fs.existsSync(filePath)) {
+     res.send({"reply":'',msg:'file is exist try to diffrent name'})
+    } else {
+    fs.writeFileSync(filePath,html)
+    res.send({"reply":fileName})
+    }
+
+}
+export function htmlFile(req,res){
+    const fileName=req.params.fileName
+    const filePath=path.join(__dirname, `../html_files/${fileName}.html`)
+    res.sendFile(filePath)
+}
+export function AllHtmlPage(req,res){
+       const folderPath=path.join(__dirname, `../html_files`)
+      const files = fs.readdirSync(folderPath);
+      console.log('Files in folder:', files);
+      let str=""
+      for (let fe of files){
+      if ( fs.statSync(path.join(folderPath,fe)).isFile()){
+        const fileName=fe.split('.')[0]
+     str= str+`<p style="margin:0px"><a href="/html/file/${fileName}" target="_blank" style="margin:5px;font-size:16px">File name is <span style="color:red;
+      font-size:20px;text-decoration:underline"> ${fe}</span></a></p>`
+      }}
+    res.send(`
+        <div style="width:100%;max-width:500px;background: linear-gradient(to right,#ccc,#fff,#000, #fff, blue);border-radius:10px;padding:10px;margin:auto">
+           ${str}
+        </div>
+        `)
 }
