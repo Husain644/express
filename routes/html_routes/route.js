56f8bc7e5d2ca70000face756c8d5cf76c4b70dd
husain <husain68644@gmail.com>
2025-08-29 10:34:17 +0530

    commit

---
+++ routes/html_routes/route.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import path from 'path'
+import { upload } from '../../middleware/multer.js';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename)
+import { AddHtmlFile,getHtml,htmlFile,AllFilesApi,uploadPic,uploadHtml,getAllCategories,getFile,getReactFile
+} from '../../controllers/html_controllers/control.js';
+
+const reactBuildPath = path.join(__dirname,"../../controllers/html_controllers/","static/reactBuild/view")
+const HtmlRouter =express.Router();
+HtmlRouter.use('/static',express.static(path.join(__dirname, "../../controllers/html_controllers/", "static")));
+
+HtmlRouter.get('/add',AddHtmlFile);
+HtmlRouter.get('/folderName/:folderName/file/:fileName',htmlFile)
+HtmlRouter.get('/allFilesApi',AllFilesApi)
+HtmlRouter.post('/gethtml',upload.single('myFiles'),getHtml)
+HtmlRouter.post('/tocloudinary',upload.single('myFiles'),uploadPic)
+HtmlRouter.post('/uploadHtml',upload.single('myFiles'),uploadHtml)
+HtmlRouter.get('/categories',getAllCategories)
+HtmlRouter.get('/categories/:folderName',getAllCategories)
+HtmlRouter.get('/getFile/:folderName/:subFolder/:fileName',getFile)
+
+HtmlRouter.use("/view", express.static(reactBuildPath));
+HtmlRouter.get("/view/*", (req, res) => {
+  res.sendFile(path.join(reactBuildPath, "index.html"));
+});
+
+
+
+export default HtmlRouter;
--- routes/main.js
+++ routes/main.js
@@ -1,13 +1,11 @@
 import express from 'express'
 import userRouter from './account/routing.js';
-import HtmlRouter from '../html_upload/route.js'
+import HtmlRouter from './html_routes/route.js';
 import { NotFound } from '../utils/notFound.js';
-import { usege } from '../utils/utilsFunction.js';
 
-const router= express.Router();
-router.use('/account',userRouter);
-router.use('/html',HtmlRouter);
-router.use('/usage',usege);
-router.use('/',NotFound);
+const router = express.Router();
+router.use('/account', userRouter);
+router.use('/html', HtmlRouter);
+router.use('/', NotFound);
 
 export default router;
